d16fe9cdab92550f5b93cb72806ffd05dc3d0b7a
weighne <cvickery@posteo.net>
2022-02-07 14:40:45 -0500

    Loop and copy to clipboard

--- py_wiki.py
+++ py_wiki.py
@@ -1,4 +1,13 @@
 import wikipedia
+from os import system, name
+import pyperclip as pclip
+
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 
 def wiki_search():
@@ -8,21 +17,31 @@ def wiki_search():
     for x in range(len(search_results)):
         print(f"{x} - {search_results[x]}")
 
-    selection = input("\nSelect an article (\'q\' to search again): ").strip()
+    selection = input("\nSelect an article (\'q\' to search again or \'exit\' to exit): ").strip()
 
     return search_results, selection
 
 
 if __name__ == '__main__':
-    search_results, selection = wiki_search()
-#    print(search_results, selection)
+    selection = ""
+    while selection.lower() != 'exit':
+        search_results, selection = wiki_search()
+    #   print(search_results, selection)
 
-    if selection == 'q' or selection == 'Q':
-        wiki_search()
-    else:
-#        print(type(selection))
-        print()
-        print(wikipedia.summary(search_results[int(selection)]))
+        if selection.lower() == 'q':
+            wiki_search()
+        elif selection.lower() == 'exit':
+            break
+        else:
+    #       print(type(selection))
+            print()
+            summary = wikipedia.summary(search_results[int(selection)])
+            print(summary)
+            action = input("Press \'C\' to copy summary to clipboard\n Press any other key to continue...")
+            if action.lower() == 'c':
+                pclip.copy(summary)
+            else:
+                continue
 
 # print(wikipedia.search(search_term))
 
