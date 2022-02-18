e8e58fe62d69d73f00624fe2bd0d065b5c493198
weighne <cvickery@posteo.net>
2022-02-18 11:21:23 -0500

    Beginning work on parsing wikitext

--- wikipydia.py
+++ wikipydia.py
@@ -49,7 +49,7 @@ def get_sections(page):
     return sections
 
 
-def sections_choice(sections):
+def sections_choice(sections,z):
     '''
     Sections are indexed in order of appearance on the page, so we list the section titles, but just use the user's input (1, 2, 3, etc.) to call the specific section
     This feels very silly, but seems to work pretty well... so I'm gonna leave it alone.
@@ -65,22 +65,46 @@ def sections_choice(sections):
 
     formatted_url = section_url.format(sections.json()["parse"]["title"].replace(" ","_"),user_choice)
     section_get = requests.get(formatted_url)
-    with open("section_dump.json","w") as out_file:
+    with open(f"section_dump-{z}.json","w") as out_file:
         json.dump(section_get.json(), out_file, indent=4)
     # print(formatted_url)
     return section_get.json()
 
 
+def parse_text(section):
+    special_chars = ['\n','<ref>','</ref>','{{','}}','[[',']]','','','','']
+    ref_string = "<ref>(.*?)</ref>"  # regex for reference string
+    # TODO: use regex to find unwanted ref strings and remove them from the plain text
+    # TODO: use regex to find special strings [[]] / {{}} and then handle them accordingly
+    plain_text = []
+    skip=0
+    for x in section.split(" "):
+        if x in special_chars:
+            if x == "<ref>":
+                skip=1
+            elif x == "</ref>":
+                skip=0
+            elif x == "{{convert":
+                print("do math")
+            else:
+                continue
+        elif x not in special_chars and skip != 1:
+            plain_text.append(x)
+
+
 if __name__ == "__main__":
     # search_term = input("Enter your search term: ")
-    search_term = "Bees"  # testing
+    z=0
+    while z<=20:
+        search_term = "Bees"  # testing
 
-    page_title = wiki_search(search_term, 10)
-    # print(page_title)
-    wiki_page = requests.get(page_url.format(page_title))
-    # print(wiki_page)
-    page_content = wiki_plain(wiki_page)
-    # print(page_content)
-    wiki_sections = get_sections(page_title)
-    # print(wiki_sections.json())
-    print(sections_choice(wiki_sections))
+        page_title = wiki_search(search_term, 10)
+        # print(page_title)
+        wiki_page = requests.get(page_url.format(page_title))
+        # print(wiki_page)
+        page_content = wiki_plain(wiki_page)
+        # print(page_content)
+        wiki_sections = get_sections(page_title)
+        # print(wiki_sections.json())
+        print(sections_choice(wiki_sections,z))
+        z+=1
